50c3fde0395bd868ce0635f3d22a6add2bed71df
Mattlau04 <37871312+Mattlau04@users.noreply.github.com>
2019-10-05 16:44:39 +0200

    Update Coinbombleechsafe.py

--- Coinbombleechsafe.py
+++ Coinbombleechsafe.py
@@ -20,7 +20,7 @@ async def on_message(message):
     if "<:coinbomb:573152290374942720> ***" == message.content[:34] and "Type `I WANT MONEY` into the chat now if you want to collect coins from their coin bomb!" in message.content and "has thrown a coin bomb!***" in message.content and message.author.id == 270904126974590976:
         date = datetime.datetime.now()
         print ("Found a coin bomb at " + str(date.hour) + "h" + str(date.minute))
-        time = "0." + str(randint(2000,5000))
+        time = "0." + str(randint(3000,7500))
         sleep(float(time))
         await message.channel.send("i want money")
 
