7198e0d75b20a855d7e4fdaf777f5a7022380621
Mattlau04 <37871312+Mattlau04@users.noreply.github.com>
2019-10-05 15:36:24 +0200

    Add files via upload

---
+++ Coinbombleechperfect.py
@@ -0,0 +1,30 @@
+import discord
+from random import randint
+from time import sleep
+from discord import ext
+from discord.ext import commands
+import asyncio
+import sys
+import datetime
+
+client = discord.Client()
+token = "urtokenhere"
+
+@client.event
+async def on_ready():
+    print ("Logged in")
+    print ("--------------------------")
+
+@client.event
+async def on_message(message):
+    if "<:coinbomb:573152290374942720> ***" == message.content[:34] and "Type `I WANT MONEY` into the chat now if you want to collect coins from their coin bomb!" in message.content and "has thrown a coin bomb!***" in message.content and message.author.id == 270904126974590976:
+        date = datetime.datetime.now()
+        if randint(1,3) == 3:
+            print ("[Collected] Found a coin bomb at " + str(date.hour) + "h" + str(date.minute))
+            time = "0." + str(randint(3000,7500))
+            sleep(float(time))
+            await message.channel.send("i want money")
+        else:
+            print ("[Not collected] Found a coin bomb at " + str(date.hour) + "h" + str(date.minute))
+
+client.run(token, bot=False)
